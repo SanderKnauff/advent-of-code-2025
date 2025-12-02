9eea519f66fbaccd695b68f9845caadf2582f2a8
Sander Knauff <sanderknauff@hotmail.com>
2025-12-02 20:36:59 +0100

    Boilerplate: added argument parsing to main method so a single day can be run

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::env;
+
 mod day1;
 mod day2;
 
 fn main() {
-    day1::run();
-    day2::run();
+    let mut args = env::args();
+    let first_argument = args.nth(1);
+
+    let first_argument = match first_argument {
+        Some(arg) => arg,
+        None => {
+            println!("Please enter a day number");
+            return;
+        }
+    };
+
+    match first_argument.as_str() {
+        "1" => day1::run(),
+        "2" => day2::run(),
+        n => println!("Unknown day: {}", n),
+    }
 }
